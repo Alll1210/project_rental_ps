f8fc96e91f8464f23295114a8c2953368e45fd54
Alll1210 <izunauciha011@gmail.com>
2023-11-23 09:44:18 +0700

    update dashboard

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
+  final List<String> playstationList = [
+    'PlayStation Portable (PSP)',
+    'PlayStation 1',
+    'PlayStation 2',
+    'PlayStation 3',
+    'PlayStation 4',
+    'PlayStation 5',
+    // Tambahkan lebih banyak item sesuai kebutuhan
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('PlayStation Rentals'),
-        backgroundColor: Colors.blue, // Ubah warna app bar sesuai keinginan
+        backgroundColor: Colors.blue,
       ),
       body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: playstationList.length,
+          itemBuilder: (context, index) {
+            return buildPlayStationItem(context, playstationList[index], index);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildPlayStationItem(BuildContext context, String playstation, int index) {
+    // Membuat path untuk gambar berdasarkan index
+    String imagePath = 'assets/ps_image$index.jpg';
+
+    return Card(
+      elevation: 8.0,
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Text(
-              'PlayStation 5',
+              playstation,
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
@@ -23,19 +52,18 @@ class Dashboard extends StatelessWidget {
             ),
             SizedBox(height: 10),
             Image.asset(
-              'assets/ps_image.jpg',
-              height: 150, // Sesuaikan tinggi gambar sesuai kebutuhan
-              width: 150, // Sesuaikan lebar gambar sesuai kebutuhan
+              imagePath,
+              height: 150,
+              width: 150,
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Implement "cek" button logic
-                // You can add specific actions or navigate to another page
-                print('Cek button pressed');
+                // Implement "cek" button logic for the specific PlayStation
+                print('Cek button pressed for $playstation');
               },
               style: ElevatedButton.styleFrom(
-                primary: Colors.green, // Ubah warna tombol sesuai keinginan
+                primary: Colors.green,
               ),
               child: Text('Cek'),
             ),
--- lib/register_page.dart
+++ lib/register_page.dart
@@ -6,34 +6,67 @@ class RegisterPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Register'),
+        backgroundColor: Colors.orange, // Ubah warna app bar sesuai keinginan
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Username'),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.orange, Colors.pink], // Ubah warna gradient sesuai keinginan
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Card(
+              elevation: 8.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        icon: Icon(Icons.person),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(Icons.email),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        icon: Icon(Icons.lock),
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Implement register logic
+                        // Navigate to dashboard after successful registration
+                        Navigator.pushReplacementNamed(context, '/dashboard');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.orange, // Ubah warna tombol sesuai keinginan
+                      ),
+                      child: Text('Register'),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            SizedBox(height: 10),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Implement register logic
-                // Navigate to dashboard after successful registration
-                Navigator.pushReplacementNamed(context, '/login');
-              },
-              child: Text('Register'),
-            ),
-          ],
+          ),
         ),
       ),
     );
