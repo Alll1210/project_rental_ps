45136df18f868e6e1c544752a117aac627fb580c
Alll1210 <izunauciha011@gmail.com>
2023-11-23 08:38:06 +0700

    update tampilan login, dashboard

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -6,22 +6,42 @@ class Dashboard extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('PlayStation Rentals'),
+        backgroundColor: Colors.blue, // Ubah warna app bar sesuai keinginan
       ),
-      body: PSList(),
-    );
-  }
-}
-
-class PSList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Implement logic to fetch and display PlayStation list
-    return ListView(
-      children: [
-        ListTile(title: Text('PlayStation 4')),
-        ListTile(title: Text('PlayStation 5')),
-        // Add more items as needed
-      ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'PlayStation 5',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+            ),
+            SizedBox(height: 10),
+            Image.asset(
+              'assets/ps_image.jpg',
+              height: 150, // Sesuaikan tinggi gambar sesuai kebutuhan
+              width: 150, // Sesuaikan lebar gambar sesuai kebutuhan
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Implement "cek" button logic
+                // You can add specific actions or navigate to another page
+                print('Cek button pressed');
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // Ubah warna tombol sesuai keinginan
+              ),
+              child: Text('Cek'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,38 +6,71 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
+        backgroundColor: Colors.blue, // Ubah warna app bar sesuai keinginan
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Email'),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue, Colors.teal], // Ubah warna gradient sesuai keinginan
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Card(
+              elevation: 8.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(Icons.email),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        icon: Icon(Icons.lock),
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Implement login logic
+                        // Navigate to dashboard after successful login
+                        Navigator.pushReplacementNamed(context, '/dashboard');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue, // Ubah warna tombol sesuai keinginan
+                      ),
+                      child: Text('Login'),
+                    ),
+                    SizedBox(height: 10),
+                    TextButton(
+                      onPressed: () {
+                        // Navigate to register page
+                        Navigator.pushNamed(context, '/register');
+                      },
+                      child: Text(
+                        'Don\'t have an account? Register here',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            SizedBox(height: 10),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Implement login logic
-                // Navigate to dashboard after successful login
-                Navigator.pushReplacementNamed(context, '/dashboard');
-              },
-              child: Text('Login'),
-            ),
-            SizedBox(height: 10),
-            TextButton(
-              onPressed: () {
-                // Navigate to register page
-                Navigator.pushNamed(context, '/register');
-              },
-              child: Text('Don\'t have an account? Register here'),
-            ),
-          ],
+          ),
         ),
       ),
     );
